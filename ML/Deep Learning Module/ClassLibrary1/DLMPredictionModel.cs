bf8922500b9e73dd238c2651306478b448b57245
Obrelix <ialimpertis@outlook.com>
2023-02-27 22:04:37 +0200

    Created a class library named as DeltaForce.OpenAI.DeepLearingModelCL. The above cl contains a trained deep learning model on plant diseases and a class for model consumption. Also created the coresponting nuget package for the above cl.

---
+++ ML/Deep Learning Module/ClassLibrary1/DLMPredictionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaForce.OpenAI.DeepLearingModelCL {
+    public class DLMPredictionModel {
+        public string LeafType { get; set; }
+        public string LeafStatus { get; set; }
+        public string ScorePerc { get; set; }
+        public float Score { get; set; }
+        public string GPT3ProposedTreatment { get; set; }
+    }
+}
---
+++ ML/Deep Learning Module/ClassLibrary1/DLMPredictions.cs
@@ -0,0 +1,36 @@
+﻿
+using DeltaForce_OpenAI_DeepLearingModelCL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaForce.OpenAI.DeepLearingModelCL {
+    public  class DLMPredictions {
+        public List<DLMPredictionModel>? GetPrediction(string base64Image) {
+            if (base64Image == null || string.IsNullOrEmpty(base64Image)) return null;
+            List<DLMPredictionModel> returnList = new List<DLMPredictionModel>();
+            var input = new DLModel.ModelInput() {
+                ImageSource = Convert.FromBase64String(base64Image)
+            };
+            var sortedScoresWithLabel = DLModel.PredictAllLabels(input);
+            if (!sortedScoresWithLabel.Any()) return null;
+            foreach (var score in sortedScoresWithLabel) {
+                if (score.Value < 0.01) continue;
+                DLMPredictionModel dLMPrediction = new DLMPredictionModel() {
+                    LeafType = score.Key.Split("___").First().Replace('_', ' ').Replace(",", string.Empty),
+                    LeafStatus = score.Key.Split("___").Last().Replace('_', ' '),
+                    ScorePerc = $"{Math.Round(score.Value * 100, 2)} %",
+                    Score = (float)Math.Round(score.Value, 4)
+                };
+
+                returnList.Add(dLMPrediction);
+            }
+            return returnList;
+        }
+
+    }
+}
