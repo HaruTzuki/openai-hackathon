178c41a4bdb8d1c18ad4aaf7fdd456f8ad921c66
Trelis Charalampos <xtrelis@yahoo.gr>
2023-02-28 17:44:12 +0200

    Fix Web Api & Get Predictions

--- WepApi/WebApi/WebApi/Controllers/MyController.cs
+++ WepApi/WebApi/WebApi/Controllers/MyController.cs
@@ -5,6 +5,12 @@ using OpenAI.GPT3;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels;
 using System.Text;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using DeltaForce.OpenAI.DeepLearingModelCL;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json;
+using System.Net;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace WebApi.Controllers {
     [ApiController]
@@ -17,71 +23,61 @@ namespace WebApi.Controllers {
             _logger = logger;
         }
 
-        [HttpPost(Name = "Post")]
-        public async Task<IActionResult> OnPostDoAsync([FromForm]IFormFile file) {
-            if (file.Length > 0) {
-                var filePath = Path.GetTempFileName();
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, "Ordered Predictions", typeof(List<DLMPredictionModel>))]
+        public async Task<IActionResult> Get([FromBody] string base64Image) {
+            if (string.IsNullOrEmpty(base64Image)) return BadRequest();
 
-                using (var stream = System.IO.File.Create(filePath)) {
-                    await file.CopyToAsync(stream);
+            if (base64Image.Length > 0) {
+                //var filePath = Path.GetTempFileName();
+
+                //using (var stream = System.IO.File.Create(filePath)) {
+                //    await file.CopyToAsync(stream);
+                //}
+
+                //string base64Str = string.Empty;
+                //using (var ms = new MemoryStream()) {
+                //    file.CopyTo(ms);
+                //    var fileBytes = ms.ToArray();
+                //    base64Str = Convert.ToBase64String(fileBytes);
+                //}
+
+                if (string.IsNullOrEmpty(base64Image)) return BadRequest();
+
+                var gpt3 = new OpenAIService(new OpenAiOptions() {
+                    ApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!
+                });
+
+                DLMPredictions dLMPredictions = new DLMPredictions();
+                var predictions = dLMPredictions.GetPrediction(base64Image);
+                foreach (var prediction in predictions!) {
+                    if (prediction.Score > 0.25 && !prediction.LeafStatus.Equals("healthy")) {
+                        var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest() {
+                            Prompt = $"Write possible treatments for {prediction.LeafStatus} on {prediction.LeafType} plant",
+                            Model = Models.TextDavinciV3,
+                            Temperature = 0.1F,
+                            MaxTokens = 100
+                        });
+
+                        StringBuilder sb = new StringBuilder();
+                        if (completionResult.Successful) {
+                            foreach (var choice in completionResult.Choices) {
+                                prediction.GPT3ProposedTreatment = choice.Text;
+                            }
+                        } else {
+                            if (completionResult.Error == null) {
+                                prediction.GPT3ProposedTreatment = "Unknown Error";
+
+                            } else {
+                                prediction.GPT3ProposedTreatment = $"{completionResult.Error.Code}: {completionResult.Error.Message}";
+                            }
+                        }
+                    } else if (prediction.LeafStatus.Equals("healthy")) prediction.GPT3ProposedTreatment = "No treatment needed.";
                 }
+                return Content(JsonConvert.SerializeObject(predictions));
             }
 
-            // john job
-
-            var gpt3 = new OpenAIService(new OpenAiOptions() {
-                ApiKey = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["ChatGPTApiKey"]
-            });
-
-            var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest() {
-                Prompt = "John Response",
-                Model = Models.TextDavinciV2,
-                Temperature = 0.5F,
-                MaxTokens = 100
-            });
-
-            StringBuilder sb = new StringBuilder();
-            if (completionResult.Successful) {
-                foreach (var choice in completionResult.Choices) {
-                    sb.Append(choice.Text + Environment.NewLine);
-                }
-            } else {
-                if (completionResult.Error == null) {
-                    sb.Append("Unknown Error");
-
-                } else {
-                    sb.Append($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-                }
-            }
-
-            return Ok(sb.ToString());
+            return BadRequest();
         }
-
-
-        //[HttpPost(Name = "ChatGPTTest")]
-        //public async Task<IActionResult> ChatGPTTest() {
-        //    var gpt3 = new OpenAIService(new OpenAiOptions() {
-        //        ApiKey = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["ChatGPTApiKey"]
-        //    });
-
-        //    var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest() {
-        //        Prompt = "What is the meaning of life?",
-        //        Model = Models.TextDavinciV2,
-        //        Temperature = 0.5F,
-        //        MaxTokens = 100
-        //    });
-
-        //    if (completionResult.Successful) {
-        //        foreach (var choice in completionResult.Choices) {
-        //            Console.WriteLine(choice.Text);
-        //        }
-        //    } else {
-        //        if (completionResult.Error == null) {
-        //            throw new Exception("Unknown Error");
-        //        }
-        //        Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-        //    }
-        //    return Ok();
-        //}
     }
 }
