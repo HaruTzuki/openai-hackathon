6a1d257face627154835afff397c4206f8e98b44
Trelis Charalampos <xtrelis@yahoo.gr>
2023-03-01 13:08:27 +0200

    Web Api Add Get Method with IFormFile Arg

--- WepApi/WebApi/WebApi/Controllers/MyController.cs
+++ WepApi/WebApi/WebApi/Controllers/MyController.cs
@@ -25,31 +25,29 @@ namespace WebApi.Controllers {
 
         [HttpGet]
         [SwaggerResponse((int)HttpStatusCode.OK, "Ordered Predictions", typeof(List<DLMPredictionModel>))]
-        public async Task<IActionResult> Get([FromBody] string base64Image) {
-            if (string.IsNullOrEmpty(base64Image)) return BadRequest();
+        public async Task<IActionResult> Get([FromBody] IFormFile file) {
+            if (file == null) return BadRequest();
 
-            if (base64Image.Length > 0) {
+            if (file.Length > 0) {
                 //var filePath = Path.GetTempFileName();
 
                 //using (var stream = System.IO.File.Create(filePath)) {
                 //    await file.CopyToAsync(stream);
                 //}
 
-                //string base64Str = string.Empty;
-                //using (var ms = new MemoryStream()) {
-                //    file.CopyTo(ms);
-                //    var fileBytes = ms.ToArray();
-                //    base64Str = Convert.ToBase64String(fileBytes);
-                //}
-
-                if (string.IsNullOrEmpty(base64Image)) return BadRequest();
+                string base64Str = string.Empty;
+                using (var ms = new MemoryStream()) {
+                    file.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    base64Str = Convert.ToBase64String(fileBytes);
+                }
 
                 var gpt3 = new OpenAIService(new OpenAiOptions() {
                     ApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!
                 });
 
                 DLMPredictions dLMPredictions = new DLMPredictions();
-                var predictions = dLMPredictions.GetPrediction(base64Image);
+                var predictions = dLMPredictions.GetPrediction(base64Str);
                 foreach (var prediction in predictions!) {
                     if (prediction.Score > 0.25 && !prediction.LeafStatus.Equals("healthy")) {
                         var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest() {
@@ -79,5 +77,60 @@ namespace WebApi.Controllers {
 
             return BadRequest();
         }
+
+        //[HttpGet]
+        //[SwaggerResponse((int)HttpStatusCode.OK, "Ordered Predictions Test", typeof(List<DLMPredictionModel>))]
+        //public async Task<IActionResult> GetTest([FromBody] string base64Image) {
+        //    if (string.IsNullOrEmpty(base64Image)) return BadRequest();
+
+        //    if (base64Image.Length > 0) {
+        //        //var filePath = Path.GetTempFileName();
+
+        //        //using (var stream = System.IO.File.Create(filePath)) {
+        //        //    await file.CopyToAsync(stream);
+        //        //}
+
+        //        //string base64Str = string.Empty;
+        //        //using (var ms = new MemoryStream()) {
+        //        //    file.CopyTo(ms);
+        //        //    var fileBytes = ms.ToArray();
+        //        //    base64Str = Convert.ToBase64String(fileBytes);
+        //        //}
+
+        //        var gpt3 = new OpenAIService(new OpenAiOptions() {
+        //            ApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!
+        //        });
+
+        //        DLMPredictions dLMPredictions = new DLMPredictions();
+        //        var predictions = dLMPredictions.GetPrediction(base64Image);
+        //        foreach (var prediction in predictions!) {
+        //            if (prediction.Score > 0.25 && !prediction.LeafStatus.Equals("healthy")) {
+        //                var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest() {
+        //                    Prompt = $"Write possible treatments for {prediction.LeafStatus} on {prediction.LeafType} plant",
+        //                    Model = Models.TextDavinciV3,
+        //                    Temperature = 0.1F,
+        //                    MaxTokens = 100
+        //                });
+
+        //                StringBuilder sb = new StringBuilder();
+        //                if (completionResult.Successful) {
+        //                    foreach (var choice in completionResult.Choices) {
+        //                        prediction.GPT3ProposedTreatment = choice.Text;
+        //                    }
+        //                } else {
+        //                    if (completionResult.Error == null) {
+        //                        prediction.GPT3ProposedTreatment = "Unknown Error";
+
+        //                    } else {
+        //                        prediction.GPT3ProposedTreatment = $"{completionResult.Error.Code}: {completionResult.Error.Message}";
+        //                    }
+        //                }
+        //            } else if (prediction.LeafStatus.Equals("healthy")) prediction.GPT3ProposedTreatment = "No treatment needed.";
+        //        }
+        //        return Content(JsonConvert.SerializeObject(predictions));
+        //    }
+
+        //    return BadRequest();
+        //}
     }
 }
